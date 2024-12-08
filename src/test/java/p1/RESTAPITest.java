b1dafae0b1a24feb3fa58e7c5d9a081a46bd701b
Vasanth Kumar <vasu.bellavi@gmail.com>
2024-12-08 10:56:54 +0530

    this is commit from Asian time zone guy

---
+++ src/test/java/p1/RESTAPITest.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class RESTAPITest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rest API Tests1");
+		System.out.println("Rest API Tests2");
+		System.out.println("Rest API Tests2");
+
+	}
+
+}
